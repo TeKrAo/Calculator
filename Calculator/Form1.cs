02d1d099dd55c789fe8dbffcda1bb27ec969476d
TeKrAo <rati.chkhikvadze@gmail.com>
2024-02-06 23:51:17 +0400

    initial commit

---
+++ Calculator/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string CalTotal;
+        int num1;
+        int num2;
+        string option;
+        int result;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn1.Text;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn2.Text;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn3.Text;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn4.Text;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn5.Text;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn6.Text;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn7.Text;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn8.Text;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn9.Text;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + btn0.Text;
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            option = "+";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            option = "-";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnMultiplation_Click(object sender, EventArgs e)
+        {
+            option = "X";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            option = "/";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTotal.Clear();
+        }
+
+        private void btnEql_Click(object sender, EventArgs e)
+        {
+            num2 = int.Parse(txtTotal.Text);
+
+            if(option == ("+"))
+            {
+                result = num1 + num2;
+            }
+            else if (option == ("-"))
+            {
+                result = num1 - num2;
+            }
+            else if (option == ("X"))
+            {
+                result = num1 * num2;
+            }
+            else if (option == ("/"))
+            {
+                result = num1 / num2;
+            }
+
+            txtTotal.Text = result + "";
+            }
+        }
+    }
+    
